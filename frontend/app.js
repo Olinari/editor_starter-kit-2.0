d255e431f702983ffad44c6118fb8903a9348e61
Dar Magall <ariel0lin@gmail.com>
2022-09-19 02:55:14 +0300

    hi

--- frontend/app.js
+++ frontend/app.js
@@ -1,28 +1,30 @@
 class ReceiptsManager {
-   constructor() {}
-   getAllReceipts() {
-      const allReceipts = {};
-      const response = fetch('./getAllReceipts', {
-         method: 'GET',
-      }).then((res) =>
-         res
-            .json()
-            .then(
-               (data) => (allReceipts.data = JSON.parse(data.body).documents)
-            )
-      );
-      return response;
-   }
+    constructor() {}
+    getAllReceipts() {
+        const allReceipts = {};
+        const response = fetch('./getAllReceipts', {
+            method: 'GET',
+        }).then((res) =>
+            res
+                .json()
+                .then(
+                    (data) =>
+                        (allReceipts.data = JSON.parse(data.body).documents)
+                )
+        );
+        return response;
+    }
 
-   createReceiptsTable(data) {
-      console.table(data[1]);
-      const body = document.querySelector('body');
-      const oldTable = document.querySelector('#rTable');
-      oldTable?.remove();
-      body.insertAdjacentHTML(
-         'beforeend',
-         `<table id="rTable" class="styled-table">
+    createReceiptsTable(data) {
+        console.table(data[1]);
+        const body = document.querySelector('body');
+        const oldTable = document.querySelector('#rTable');
+        oldTable?.remove();
+        body.insertAdjacentHTML(
+            'beforeend',
+            `<table id="rTable" class="styled-table">
          <thead>
+         <th></th>
          <th>category</th>
          <th>subtotal</th>
          <th>currency</th>
@@ -30,29 +32,32 @@ class ReceiptsManager {
          </thead>
          <tbody>
          ${data
-            .reverse()
-            .map(
-               (row) => `<tr>
+             .reverse()
+             .map(
+                 (row) => `<tr>
+         <td><img width="30" src="${row.img_url}"></td>
          <td>${row.category}</td>
          <td>${row.subtotal}</td>
          <td>${row.currency_code}</td>
          <td>${row.date}</td>
-         <td><img width="30" src="${row.img_url}"></td>
+      
          </tr>`
-            )
-            .join('')}
+             )
+             .join('')}
          </tbody>
          </table>`
-      );
-   }
+        );
+    }
 }
 
 (function app() {
-   const receiptsManager = new ReceiptsManager();
-   const allReceipts = receiptsManager.getAllReceipts();
-   allReceipts.then((data) => receiptsManager.createReceiptsTable(data));
-
-   document.querySelector('#submit').onclick = () => {
-      app();
-   };
+    const receiptsManager = new ReceiptsManager();
+    const allReceipts = receiptsManager.getAllReceipts();
+    allReceipts.then((data) => receiptsManager.createReceiptsTable(data));
+    formLoader.submit('form', function (dados) {
+        console.log(dados);
+    });
+    document.querySelector('#submit').onclick = () => {
+        app();
+    };
 })();
--- index.js
+++ index.js
@@ -12,57 +12,60 @@ var ENVIRONMENT_URL = 'https://api.veryfi.com/';
 
 app.use('/frontend', express.static(__dirname + '/frontend'));
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.post('/fileupload', function (req, res) {
-   const form = new formidable.IncomingForm();
-   form.parse(req, function (err, fields, files) {
-      const file = files.img;
-      if (file) {
-         const fileName = file.oldFilename;
-         const filePath = file.filepath;
-         console.log(fileName, filePath);
-         const base64str = fs.readFileSync(filePath, 'base64');
-         console.log(base64str);
-         var jsonData = {
-            file_data: base64str,
-            file_name: fileName,
-            boost_mode: true,
-         };
-         var options = {
-            method: 'POST',
-            uri: ENVIRONMENT_URL + '/api/v8/partner/documents/',
-            headers: {
-               'Content-Type': 'application/json',
-               Accept: 'application/json',
-               'CLIENT-ID': CLIENT_ID,
-               AUTHORIZATION: `apikey ${USER_NAME}:${API_KEY}`,
-            },
-            json: jsonData,
-         };
-         request(options, function (error, response, body) {
-            console.log(error, response, body);
-            res.sendFile(path.join(__dirname, './index.html'));
-         });
-      }
-   });
+    const form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        const file = files.img;
+        if (file) {
+            const fileName = file.oldFilename;
+            const filePath = file.filepath;
+            console.log(fileName, filePath);
+            const base64str = fs.readFileSync(filePath, 'base64');
+            console.log(base64str);
+            var jsonData = {
+                file_data: base64str,
+                file_name: fileName,
+                boost_mode: true,
+            };
+            var options = {
+                method: 'POST',
+                uri: ENVIRONMENT_URL + '/api/v8/partner/documents/',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    'CLIENT-ID': CLIENT_ID,
+                    AUTHORIZATION: `apikey ${USER_NAME}:${API_KEY}`,
+                },
+                json: jsonData,
+            };
+            request(options, function (error, response, body) {
+                console.log(error, response, body);
+                res.writeHead(302, {
+                    Location: '/',
+                });
+                res.end();
+            });
+        }
+    });
 });
 
 app.get('/getAllReceipts', function (req, res) {
-   var options = {
-      method: 'GET',
-      uri: ENVIRONMENT_URL + '/api/v8/partner/documents/',
-      headers: {
-         'Content-Type': 'application/json',
-         Accept: 'application/json',
-         'CLIENT-ID': CLIENT_ID,
-         AUTHORIZATION: `apikey ${USER_NAME}:${API_KEY}`,
-      },
-   };
-   request(options, function (error, response, body) {
-      res.send(response);
-   });
+    var options = {
+        method: 'GET',
+        uri: ENVIRONMENT_URL + '/api/v8/partner/documents/',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'CLIENT-ID': CLIENT_ID,
+            AUTHORIZATION: `apikey ${USER_NAME}:${API_KEY}`,
+        },
+    };
+    request(options, function (error, response, body) {
+        res.send(response);
+    });
 });
 
 const PORT = process.env.PORT || 5500;
